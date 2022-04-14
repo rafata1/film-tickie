b2677cac22a3ffb14b319c6887f608dbafb0024f
Dung CDA <dung.cda@teko.vn>
2022-04-14 17:41:59 +0700

    update max conn to mysql

--- connectors/mysql/mysql.go
+++ connectors/mysql/mysql.go
@@ -3,6 +3,7 @@ package mysql
 import (
     "github.com/jmoiron/sqlx"
     "os"
+    "time"
 )
 
 var conn *sqlx.DB
@@ -10,6 +11,9 @@ var conn *sqlx.DB
 func Connect() error {
     var err error
     conn, err = sqlx.Connect("mysql", os.Getenv("MYSQL"))
+    conn.SetMaxIdleConns(50)
+    conn.SetMaxOpenConns(100)
+    conn.SetConnMaxLifetime(10 * time.Second)
     return err
 }
 
