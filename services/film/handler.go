d2ea0254601f11d831c3a9eca4c386913896cd7a
Dung CDA <dung.cda@teko.vn>
2022-04-14 10:59:48 +0700

    change api list films to public api

--- services/film/handler.go
+++ services/film/handler.go
@@ -2,9 +2,7 @@ package film
 
 import (
     "github.com/gin-gonic/gin"
-    "github.com/templateOfService/services/auth"
     "net/http"
-    "strings"
 )
 
 type Handler struct {
@@ -22,19 +20,9 @@ func NewHandler() *Handler {
 // @Tags         Film service
 // @Accept       json
 // @Produce      json
-// @param Authorization header string true "Authorization"
 // @Success      200
 // @Router       /api/v1/films [get]
 func (h *Handler) ListFilms(c *gin.Context) {
-    _, err := auth.GetGlobalJWTManager().VerifyToken(
-        strings.ReplaceAll(c.GetHeader("authorization"), "Bearer ", ""))
-    if err != nil {
-        c.JSON(http.StatusUnauthorized, BaseRes{
-            Message: "unauthorized",
-        })
-        return
-    }
-
     films, err := h.service.ListFilms()
     if err != nil {
         c.JSON(http.StatusOK, BaseRes{
